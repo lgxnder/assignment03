5d560db1c105dd7a47f0eb2c8c15409d55f95f5a
yutoiren <149996922+lgxnder@users.noreply.github.com>
2024-11-11 21:16:33 -0500

    +basic structure

---
+++ src/WorkHourDAO.java
@@ -0,0 +1,10 @@
+public class WorkHourDAO {
+
+	// define methods
+	// create -- add new entry 
+	// read   -- all entries or entry by id
+	// update -- modify entry 
+	// delete -- entry by id
+
+	// use SQL queries with JDBC & return success/error message
+}
---
+++ src/WorkHourEntry.java
@@ -0,0 +1,45 @@
+public class WorkHourEntry {
+	// fields
+	private String id;
+	private String name;
+	private String role;
+	private int hours;
+	
+	// constructor
+	
+
+	// getters
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public int getHours() {
+		return hours;
+	}
+
+	// setters
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+	
+}
